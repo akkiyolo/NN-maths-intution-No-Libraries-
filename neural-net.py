cf7738cfafe50124b7eea7bb6f82639af5818d15
akkiyolo <akshatshukla069@gmail.com>
2026-03-30 22:32:32 +0530

    initiated mlp layer

--- neural-net.py
+++ neural-net.py
@@ -2,3 +2,9 @@ import numpy as np
 import tqdm as tqdm
 from scipy.special import logsumexp
 from keras.datasets.mnist import load_data
+
+class MLP():
+
+  def __init__(self,din,dout):
+    self.W=(2*np.random.rand(dout,din)-1)*(np.sqrt(6)/np.sqrt(din*dout))
+    self.b=(2*np.random.rand(dout,din)-1)*(np.sqrt(6)/np.sqrt(din*dout))
